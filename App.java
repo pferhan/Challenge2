18bafdf567673d0247da6e95539376b9ab4a4d56
jlafrance3 <jlafranceiii@proton.me>
2024-04-17 20:58:37 -0400

    Created application window

--- App.java
+++ App.java
@@ -1,5 +1,27 @@
-public class App {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+
+public class App extends JFrame{
+
+    private JPanel panel;
+
+    public App() {
+
+        panel = new JPanel();
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(new Dimension(800, 600));
+        this.setContentPane(panel);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+
+        //TODO: Everything
+
+        this.setVisible(true);
+    }
+
     public static void main (String[] args) {
-        System.out.println("Start");
+        new App();
     }
 }
